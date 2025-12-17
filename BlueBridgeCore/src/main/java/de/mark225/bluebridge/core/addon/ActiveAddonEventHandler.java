2af419aabc74dcd8e3ae1822c17b9d2cd115d99d
Christian F <christian@falkner.info>
2025-12-17 18:44:05 +0100

    Make ActiveAddonEventHandler thread-safe and fix update interval unit
    Synchronized access to addedOrUpdated and deleted lists in ActiveAddonEventHandler to ensure thread safety. Also corrected the time unit in UpdateTask from MICROSECONDS to MILLISECONDS and ensured the update interval uses a long literal.

--- BlueBridgeCore/src/main/java/de/mark225/bluebridge/core/addon/ActiveAddonEventHandler.java
+++ BlueBridgeCore/src/main/java/de/mark225/bluebridge/core/addon/ActiveAddonEventHandler.java
@@ -7,25 +7,36 @@ import java.util.List;
 import java.util.function.BiConsumer;
 
 public class ActiveAddonEventHandler {
+    private static final Object LOCK = new Object();
+
     private static List<RegionSnapshot> addedOrUpdated = new ArrayList<>();
     private static List<RegionSnapshot> deleted = new ArrayList<>();
 
     public static void addOrUpdate(RegionSnapshot region) {
-        addedOrUpdated.add(region);
+        synchronized (LOCK) {
+            addedOrUpdated.add(region);
+        }
     }
 
     public static void delete(RegionSnapshot region) {
-        deleted.add(region);
-    }
-
-    public static void resetLists() {
-        addedOrUpdated = new ArrayList<>();
-        deleted = new ArrayList<>();
+        synchronized (LOCK) {
+            deleted.add(region);
+        }
     }
 
     public static void collectAndReset(BiConsumer<List<RegionSnapshot>, List<RegionSnapshot>> callback) {
-        callback.accept(addedOrUpdated, deleted);
-        resetLists();
-    }
+        final List<RegionSnapshot> added;
+        final List<RegionSnapshot> del;
 
+        synchronized (LOCK) {
+            // Listen “swappen”, damit neue Events in frische Listen gehen
+            added = addedOrUpdated;
+            del = deleted;
+            addedOrUpdated = new ArrayList<>();
+            deleted = new ArrayList<>();
+        }
+
+        // callback außerhalb des Locks ausführen
+        callback.accept(added, del);
+    }
 }
